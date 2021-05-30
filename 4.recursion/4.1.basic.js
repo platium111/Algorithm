41b26c671c10ac56475c24534a752d3602fbbf5c
Hiep Trinh <hieptq.social@gmail.com>
2021-05-30 22:57:12 +1000

    Memo for fibonanci

--- 4.recursion/4.1.basic.js
+++ 4.recursion/4.1.basic.js
@@ -10,7 +10,7 @@ function recursiveFn(params) {
   recursiveFn(params);
 }
 
-// factorial | is n!
+// 1.Normal way | factorial | is n!
 //  example 5 * 4 * 3 * 2 * 1 -> start point is 1
 function factorialFn(number) {
   if (number < 0) return NaN;
@@ -20,3 +20,28 @@ function factorialFn(number) {
   }
   return result;
 }
+
+// 2.Recursive way
+function factorialRecursive(number) {
+  if (number === 0 || number === 1) return 1;
+  return number * factorialRecursive(number - 1);
+}
+
+// 3. Fibonacci
+// example: 0 1 1 2 3 5 8...
+function fibonanciFn(n) {
+  if (n === 0) return 0;
+  if (n <= 2) return 1;
+  return fibonanciFn(n - 1) + fibonanciFn(n - 2); //imagine now we take n at the end, so this n is created by n-1 and n-2
+}
+
+// 4. Momoization recursive
+// if we see fibonanci(5) has 2 double times of fibinanci(3)
+function fibonanciMemo(n, memo = []) {
+  if (memo[n]) return memo[n];
+  if (n === 0) return 0;
+  if (n === 1) return 1;
+  return (memo[n] = fibonanciMemo(n - 1, memo) + fibonanciMemo(n - 2, memo));
+}
+
+console.log(fibonanciMemo(9));
